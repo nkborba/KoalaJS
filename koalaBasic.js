709992c2df34c7c24ea0a10f797ebcb66b4d1f8b
nborba <nicholasborba@gmail.com>
2019-11-12 16:42:18 -0300

    update

--- koalaBasic.js
+++ koalaBasic.js
@@ -1,11 +1,21 @@
 const { BrowserWindow, app } = require('electron')
+const { cp } = require('child_process')
+
+const term = cp.exec('echo OK', (err, stdout, stderr)=>{
+    if(err){
+        return err
+    }
+    else {
+        return stdout
+    }
+})
+
 var okok = require('printOk.js')
 
 function term() {
-    var ok = new okok
 
     try{
-            document.getElementById('um').innerHTML = 
+            document.getElementById('um').innerHTML = 'NOK'
     }
     catch (err) {
         document.getElementById('um').innerHTML = err
