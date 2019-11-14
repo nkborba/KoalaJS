2a958309bf26bd0755e8ee33a023f1126dcbf771
nkborba <nicholasborba@gmail.com>
2019-11-14 01:42:50 -0300

    icon change

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { BrowserWindow, app } = require ('electron')
+const { BrowserWindow, app, Tray } = require ('electron')
 
 // new customTitlebar.Titlebar({
 //     backgroundColor: customTitlebar.Color.fromHex('#03a9f4')
@@ -7,11 +7,13 @@ const { BrowserWindow, app } = require ('electron')
 // MyTitleBar.updateTitle('KOALA - You\'re KOALified ');
 
 function createWindow () {
+    const appIcon = new Tray(__dirname+'/koalaicon_15.png')
     win = new BrowserWindow({
         width: 800,
         height:600,
         resizable:false,
         frame: false,
+        icon: __dirname+"/koalaicon_15.png",
 
         webPreferences: {
             nodeIntegration: true
--- preload.js
+++ preload.js
@@ -2,11 +2,20 @@ const path = require('path')
 const customTitlebar = require('custom-electron-titlebar');
 const { remote } = require('electron')
 const { Menu, MenuItem } = remote
+const url = require('url') 
+
+// const iconUrl = url.format({
+//     pathname: path.join(__dirname, 'src/images/koalaicon_15.png'),
+//     protocol: 'file:',
+//     slashes: true
+// })
 
 const tibleBar = new customTitlebar.Titlebar({
     backgroundColor: customTitlebar.Color.fromHex('#2c3e50'),
-    icon: path.join(__dirname, "src/images/koalaicon_15.png")
+    icon: "/koalaicon_15.png"
 });
+
+// console.log(path.resolve(__dirname, "src", "images", "koalaicon_15.png"))
 
 const menu = new Menu()
 
@@ -28,4 +37,6 @@ menu.append(new MenuItem({
     ]
 }))
 
+
 tibleBar.updateMenu(menu)
+tibleBar.updateIcon('koalaicon_15.png')
